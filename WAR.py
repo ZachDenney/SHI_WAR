d84f97da0f82690221d34eff05b31c0716f9dd99
ZachDenney <52335266+ZachDenney@users.noreply.github.com>
2019-07-03 19:00:04 -0500

    Update WAR.py

--- WAR.py
+++ WAR.py
@@ -105,7 +105,7 @@ with open("war.html", "w") as f:
     f.close()
 
 next_week_planned.findOutlook()
-time.sleep(.1)
+time.sleep(3)
 next_week_planned.next_week_planned()
 frequency = 2500  # Set Frequency To 2500 Hertz
 duration = 500  # Set Duration To 1000 ms == 1 second
